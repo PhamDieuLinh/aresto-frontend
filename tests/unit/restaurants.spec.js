41bd8a2cd48860ab13b127ac392dcb26263b0819
Thuong Hoang <Thuong.@htw.com>
2023-01-08 23:21:44 +0100

    tests

--- tests/unit/restaurants.spec.js
+++ tests/unit/restaurants.spec.js
@@ -1,11 +1,8 @@
 import { mount } from '@vue/test-utils'
 import RVerwaltenView from '@/views/RVerwaltenView'
 import RErstellen from '@/components/RErstellen'
-import RBearbeiten from '@/components/RBearbeiten'
-import BewertungenView from '@/views/BewertungenView'
 
 describe('Testing Restaurants.vue', () => {
-
   it('should have rerstellen component', () => {
     // when
     const wrapper = mount(RVerwaltenView)
@@ -13,14 +10,5 @@ describe('Testing Restaurants.vue', () => {
     // then
     const erstellenComp = wrapper.findComponent(RErstellen)
     expect(erstellenComp.exists()).toBeTruthy()
-  })
-
-  it('should have persons create form component', () => {
-    // when
-    const wrapper = mount(BewertungenView)
-
-    // then
-    const bearbeitenComp = wrapper.findComponent()
-    expect(bearbeitenComp.exists()).toBeTruthy()
   })
 })
